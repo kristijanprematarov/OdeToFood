fe883f31d7c3037841f17d2dc4f55a89979c7e18
Kristijan Prematarov <kristijanprematarov@yahoo.com>
2021-04-14 15:30:57 +0200

    init

---
+++ OdeToFood.Service/InMemoryRestaurantData.cs
@@ -0,0 +1,29 @@
+﻿using OdeToFood.Models;
+using OdeToFood.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Service
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> _restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            { 
+                new Restaurant { Id = 1, Name = "KPR Snacks"},
+                new Restaurant { Id = 2, Name = "KPR Pizzas"},
+                new Restaurant { Id = 3, Name = "KPR Protein"}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAllRestaurants()
+        {
+            return _restaurants.AsEnumerable();
+        }
+    }
+}
---
+++ OdeToFood.Service/Interfaces/IRestaurantData.cs
@@ -0,0 +1,12 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Service.Interfaces
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAllRestaurants();
+    }
+}
